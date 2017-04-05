1b9ab38e0c489000b9525fbc422a627e943775cd
peter3110 <peter3110>
2017-04-05 10:30:26 -0300

    update2

--- public/js/unu_sliders.js
+++ public/js/unu_sliders.js
@@ -1,19 +1,19 @@
 function sumSlider1() {
-	$.get( "http://acciones-frontend.herokuapp.com/sum_slider1_server", function( data ) {
+	$.get( "https://acciones-frontend.herokuapp.com/sum_slider1_server", function( data ) {
   		document.getElementById('slider1').innerHTML = data.val
 	});
 	displaySlider1()
 }
 
 function subtractSlider1() {
-	$.get( "http://acciones-frontend.herokuapp.com/subtract_slider1_server", function( data ) {
+	$.get( "https://acciones-frontend.herokuapp.com/subtract_slider1_server", function( data ) {
   		document.getElementById('slider1').innerHTML = data.val
 	});
 	displaySlider1()
 }
 
 function displaySlider1() {
-	$.get( "http://acciones-frontend.herokuapp.com/slider1_server", function( data ) {
+	$.get( "https://acciones-frontend.herokuapp.com/slider1_server", function( data ) {
   		document.getElementById('slider1').innerHTML = data.val
 	});
 }
