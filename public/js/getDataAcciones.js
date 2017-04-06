2e948370495b66e16da789f36e7604330bdee70c
peter3110 <peter3110>
2017-04-06 09:23:23 -0300

    sdf

--- public/js/getDataAcciones.js
+++ public/js/getDataAcciones.js
@@ -1,8 +1,6 @@
 
 function getData() {
-    app.get('/getData', (req, res, next) => {
-      $.get( "https://acciones-backend.herokuapp.com/getData/", function( data ) {
+    return $.get( "https://acciones-backend.herokuapp.com/getData/", function( data ) {
           return data;
       });  
-    })
 }
